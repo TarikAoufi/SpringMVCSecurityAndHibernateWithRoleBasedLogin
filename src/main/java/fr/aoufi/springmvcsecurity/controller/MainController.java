a5fdf4e89c4a16b38434d99a38cc8da9fc75af72
Tarik AOUFI <t_aoufi@hotmail.com>
2020-03-22 17:52:01 +0100

    session timeout + target url by user role

--- src/main/java/fr/aoufi/springmvcsecurity/controller/MainController.java
+++ src/main/java/fr/aoufi/springmvcsecurity/controller/MainController.java
@@ -1,15 +1,12 @@
 package fr.aoufi.springmvcsecurity.controller;
 
-import fr.aoufi.springmvcsecurity.model.Role;
-import fr.aoufi.springmvcsecurity.model.User;
-import fr.aoufi.springmvcsecurity.service.RoleService;
-import fr.aoufi.springmvcsecurity.service.UserService;
-import fr.aoufi.springmvcsecurity.validator.UserFormValidator;
 import java.util.List;
 import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,18 +21,27 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import fr.aoufi.springmvcsecurity.model.Role;
+import fr.aoufi.springmvcsecurity.model.User;
+import fr.aoufi.springmvcsecurity.service.RoleService;
+import fr.aoufi.springmvcsecurity.service.UserService;
+import fr.aoufi.springmvcsecurity.validator.UserFormValidator;
 
 @Controller
 @SessionAttributes({"user_roles"})
 public class MainController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	static final String LOGGEDONUSER = "loggedonuser";
+	static final String REGISTRATION = "registration";
 	
 	@Autowired
 	UserFormValidator userFormValidator;
@@ -51,23 +57,23 @@ public class MainController {
 	
 	/**
 	 * Pour stocker les informations de connexion persistantes 
-	 * dans une table de la base de données ('PERSISTENT_LOGINS') entre les sessions.
+	 * dans une table de la base de donnÃ©es ('PERSISTENT_LOGINS') entre les sessions.
 	 */
 	@Autowired
 	PersistentTokenBasedRememberMeServices persistentTokenBasedRememberMeServices;
 	
 	/**
-	 * Pour évaluer les jetons d'authentification
+	 * Pour Ã©valuer les jetons d'authentification
 	 */
 	@Autowired
 	AuthenticationTrustResolver authenticationTrustResolver;
 	
 	/**
-	 * Pour contrôler les champs du formulaire d'inscription
-	 * @InitBinder: Permet d'dentifier les méthodes qui initialisent le WebDataBinder.
-	 * WebDataBinder: est un DataBinder qui lie le paramètre de requête aux objets JavaBean.
-	 * Il fournit des méthodes pour assigner nos classes de validateurs. 
-	 * En utilisant les méthodes addValidators () et setValidator (), nous pouvons 
+	 * Pour contrÃ´ler les champs du formulaire d'inscription
+	 * @InitBinder: Permet d'dentifier les mÃ©thodes qui initialisent le WebDataBinder.
+	 * WebDataBinder: est un DataBinder qui lie le paramÃ¨tre de requÃªte aux objets JavaBean.
+	 * Il fournit des mÃ©thodes pour assigner nos classes de validateurs. 
+	 * En utilisant les mÃ©thodes addValidators () et setValidator (), nous pouvons 
 	 * assigner nos instances de validateurs.
 	 */
 	@InitBinder
@@ -75,7 +81,7 @@ public class MainController {
 		binder.setValidator(userFormValidator);
 	}
 
-	@RequestMapping(value = {"/", "/welcome"}, method = {RequestMethod.GET})
+	@GetMapping(value = {"/", "/welcome"})
 	public String welcomePage(Model model) {
 		model.addAttribute("title", "Welcome");
 		model.addAttribute("message", "C'est la page d'accueil!");
@@ -83,96 +89,96 @@ public class MainController {
 	}
 	
 	/**
-	 * Cette méthode permet d'ajouter un nouvel utilisateur.
+	 * Cette mÃ©thode permet d'ajouter un nouvel utilisateur.
 	 */
-	@RequestMapping(value = {"/newuser", "/userregist"}, method = {RequestMethod.GET})
+	@GetMapping(value = {"/newuser", "/userregist"})
 	public String newUser(ModelMap model) {
 		User user = new User();
 		model.addAttribute("user", user);
 		model.addAttribute("edit", false);
-		model.addAttribute("loggedonuser", getPrincipal());
-		return "registration";
+		model.addAttribute(LOGGEDONUSER, getPrincipal());
+		return REGISTRATION;
 	}
 	
 	/**
-	 * Cette méthode sera appelée lors de la soumission du formulaire, en gérant la requête POST pour 
-	 * enregistrer l'utilisateur dans la bd. Il valide également l'entrée de l'utilisateur
+	 * Cette mÃ©thode sera appelÃ©e lors de la soumission du formulaire, en gÃ©rant la requÃªte POST pour 
+	 * enregistrer l'utilisateur dans la bd. Il valide Ã©galement l'entrÃ©e de l'utilisateur
 	 */
-	@RequestMapping(value = {"/newuser", "/userregist"}, method = {RequestMethod.POST})
+	@PostMapping(value = {"/newuser", "/userregist"})
 	public String saveUser(@Valid User user, BindingResult result, ModelMap model) {
 		if (result.hasErrors()) {
-			logger.info("Retour à la page registration.jsp");
-			return "registration";
+			logger.info("Retour Ã  la page registration.jsp");
+			return REGISTRATION;
 		} else {
 			userService.saveUser(user);
-			model.addAttribute("success", messageSource.getMessage("", new String[]{user.getUsername()}, Locale.getDefault()) + " "
+			model.addAttribute("success", messageSource.getMessage("", new String[]{user.getUserName()}, Locale.getDefault()) + " "
 							+ user.getFirstName() + " " + user.getLastName() + " ");
-			model.addAttribute("loggedonuser", getPrincipal());
+			model.addAttribute(LOGGEDONUSER, getPrincipal());
 			return "registrationsuccess";
 		}
 	}
 	
 	/**
-	 * Cette méthode permet de mettre à jour un utilisateur existant.
+	 * Cette mÃ©thode permet de mettre Ã  jour un utilisateur existant.
 	 */
-	@RequestMapping(value = {"/edit-user-{username}"}, method = {RequestMethod.GET})
+	@GetMapping(value = {"/edit-user-{username}"})
 	public String editUser(@PathVariable String username, ModelMap model) {
-		User user = userService.findByUname(username);
+		User user = userService.findByUserName(username);
 		model.addAttribute("user", user);
 		model.addAttribute("edit", true);
-		model.addAttribute("loggedonuser", getPrincipal());
-		return "registration";
+		model.addAttribute(LOGGEDONUSER, getPrincipal());
+		return REGISTRATION;
 	}
 	
 	/**
-	 * Cette méthode sera appelée lors de la soumission du formulaire, en gérant la requête POST pour
-	 * mettre à jour l'utilisateur dans la bd. Il valide également l'entrée de l'utilisateur
+	 * Cette mÃ©thode sera appelÃ©e lors de la soumission du formulaire, en gÃ©rant la requÃªte POST pour
+	 * mettre Ã  jour l'utilisateur dans la bd. Il valide Ã©galement l'entrÃ©e de l'utilisateur
 	 */
-	@RequestMapping(value = {"/edit-user-{username}"}, method = {RequestMethod.POST})
+	@PostMapping(value = {"/edit-user-{username}"})
 	public String updateUser(@Valid User user, BindingResult result, ModelMap model, @PathVariable String username) {
 		if (result.hasErrors()) {
-			return "registration";
+			return REGISTRATION;
 		} else {			
 			/*
-		    //décommenter ce bloc pour mettre à jour le "username".
-			if(!userService.isUserUNAMEUnique(user.getId(), user.getUsername())){
-				FieldError unameError = new FieldError("user", "username", 
-												messageSource.getMessage("registration.uniqueness.username", new String[]{user.getUsername()}, 
+		    //dÃ©commenter ce bloc pour mettre Ã  jour le "username".
+			if(!userService.isUserUNAMEUnique(user.getId(), user.getUserName())){
+				FieldError unameError = new FieldError("user", "userName", 
+												messageSource.getMessage("registration.uniqueness.username", new String[]{user.getUserName()}, 
 												Locale.getDefault())); 
 			    result.addError(unameError); 
 			    return "registration"; 
 			}
 			*/			
 			userService.updateUser(user);
-			model.addAttribute("success", messageSource.getMessage("", new String[]{user.getUsername()}, Locale.getDefault()) + " "
+			model.addAttribute("success", messageSource.getMessage("", new String[]{user.getUserName()}, Locale.getDefault()) + " "
 							+ user.getFirstName() + " " + user.getLastName() + " ");
-			model.addAttribute("loggedonuser", getPrincipal());
+			model.addAttribute(LOGGEDONUSER, getPrincipal());
 			return "updatesuccess";
 		}
 	}
 	
 	/**
-	 * Cette méthode permet de supprimer un utilisateur par son username
+	 * Cette mÃ©thode permet de supprimer un utilisateur par son username
 	 */
-	@RequestMapping(value = {"/delete-user-{username}"}, method = {RequestMethod.GET})
+	@GetMapping(value = {"/delete-user-{username}"})
 	public String deleteUser(@PathVariable String username) {
-		userService.deleteUserByUname(username);
+		userService.deleteUserByUserName(username);
 		return "redirect:/list";
 	}
 	
 	/**
 	 * Lister tous les utilisateurs
 	 */
-	@RequestMapping(value = {"/list"}, method = {RequestMethod.GET})
+	@GetMapping(value = {"/list"})
 	public String listUsers(ModelMap model) {
 		List<User>  users = userService.findAllUsers();
 		model.addAttribute("users", users);
-		model.addAttribute("loggedonuser", getPrincipal());
+		model.addAttribute(LOGGEDONUSER, getPrincipal());
 		return "userslist";
 	}
 	
 	/**
-	 * Fournir la liste des rôles d'utilisateurs aux vues
+	 * Fournir la liste des rÃ´les d'utilisateurs aux vues
 	 */
 	@ModelAttribute("roleList")
 	public List<Role> initializeUserRoles() {
@@ -180,13 +186,13 @@ public class MainController {
 	}
 	
 	/**
-	 * Cette méthode gère les requêtes GET de connexion . 
-	 * Si les utilisateurs, déjà connectés, essayent d'accéder à nouveau à la
-	 * page de connexion, ils seront redirigés vers la page qui affiche la liste des utilisateurs.
+	 * Cette mÃ©thode gÃ¨re les requÃªtes GET de connexion. 
+	 * Si les utilisateurs, dÃ©jÃ  connectÃ©s, essayent d'accÃ©der Ã  nouveau Ã  la
+	 * page de connexion, ils seront redirigÃ©s vers la page qui affiche la liste des utilisateurs.
 	 */
-	@RequestMapping(value = {"/login"}, method = {RequestMethod.GET})
+	@GetMapping(value = {"/login"})
 	public String loginPage() {
-		// Utilisateur déjà connecté ?
+		// Utilisateur dÃ©jÃ  connectÃ© ?
 		if (isCurrentAuthenticationAnonymous()) {
 			return "login";
 		} else {
@@ -195,14 +201,12 @@ public class MainController {
 	}
 	
 	/**
-	 * Gère les requêtes de déconnexion
+	 * GÃ¨re les requÃªtes de dÃ©connexion
 	 */
-	@RequestMapping(value = {"/logout"}, method = {RequestMethod.GET})
+	@GetMapping(value = {"/logout"})
 	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		if (auth != null) {
-			// new SecurityContextLogoutHandler().logout(request, response,
-			// auth);
 			persistentTokenBasedRememberMeServices.logout(request, response, auth);
 			SecurityContextHolder.getContext().setAuthentication((null) );
 		}
@@ -210,16 +214,16 @@ public class MainController {
 	}
 	
 	/**
-	 * Redirection lors d'un accès refusé
+	 * Redirection lors d'un accÃ¨s refusÃ©
 	 */
-	@RequestMapping(value = {"/Access_Denied"}, method = {RequestMethod.GET})
+	@GetMapping(value = {"/Access_Denied"})
 	public String accessDeniedPage(ModelMap model) {
-		model.addAttribute("loggedonuser", getPrincipal());
+		model.addAttribute(LOGGEDONUSER, getPrincipal());
 		return "accessDenied";
 	}
 	
 	/**
-	 * Obtenir le principal [username] de l'utilisateur actuellement connecté.
+	 * Obtenir le principal [username] de l'utilisateur actuellement connectÃ©.
 	 */
 	private String getPrincipal() {
 		String userName = null;
@@ -233,7 +237,7 @@ public class MainController {
 	}
 	
 	/**
-	 * Renvoie true si l'utilisateur est déjà authentifié, sinon false.
+	 * Renvoie true si l'utilisateur est dÃ©jÃ  authentifiÃ©, sinon false.
 	 */
 	private boolean isCurrentAuthenticationAnonymous() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
