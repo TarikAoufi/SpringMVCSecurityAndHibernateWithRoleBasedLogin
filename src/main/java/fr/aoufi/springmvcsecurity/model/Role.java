03a772a81eaed3a04a7c633551f2823108886518
Tarik AOUFI <t_aoufi@hotmail.com>
2020-03-22 18:01:12 +0100

    session timeout + target url by user role

--- src/main/java/fr/aoufi/springmvcsecurity/model/Role.java
+++ src/main/java/fr/aoufi/springmvcsecurity/model/Role.java
@@ -1,7 +1,7 @@
 package fr.aoufi.springmvcsecurity.model;
 
-import fr.aoufi.springmvcsecurity.model.RoleType;
 import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "T_ROLE")
 public class Role implements Serializable {
@@ -23,58 +26,7 @@ public class Role implements Serializable {
 	private String type;
 
 	public Role() {
-		this.type = RoleType.USER.getRoleType();
+		this.type = RoleType.USER.getRole();
 	}
-
-	public Integer getId() {
-		return this.id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getType() {
-		return this.type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Role other = (Role) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Role [id=" + id + ", type=" + type + "]";
-	}
+	
 }
