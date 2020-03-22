870d15d633fabbb97c2b530d13f04383ad208339
Tarik AOUFI <t_aoufi@hotmail.com>
2020-03-22 18:13:33 +0100

    session timeout + target url by user role

--- src/main/java/fr/aoufi/springmvcsecurity/security/SecurityConfiguration.java
+++ src/main/java/fr/aoufi/springmvcsecurity/security/SecurityConfiguration.java
@@ -15,49 +15,54 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	Logger log = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	@Qualifier("myUserDetailsService")
 	UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	PersistentTokenRepository persistentTokenRepository;
 
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-		log.info("Exécution de configureGlobalSecurity() - SecurityConfiguration");
-		auth.userDetailsService(userDetailsService);
-		auth.authenticationProvider(authenticationProvider());
+		log.info("ExÃ©cution de configureGlobalSecurity() - SecurityConfiguration");
+		auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+	}
+
+	@Bean
+	public AuthenticationSuccessHandler myAuthenticationSuccessHandler() {
+		return new MyAuthenticationSuccessHandler();
 	}
 	
 	/**
-	 * WebSecurityConfigurerAdapter fournie une configuration par défaut 
-	 * dans la méthode configure(HttpSecurity http).
+	 * WebSecurityConfigurerAdapter fournie une configuration par dÃ©faut 
+	 * dans la mÃ©thode configure(HttpSecurity http).
 	 * 
-	 * L'accès aux URL est régi comme suit:
-	 * '/' '/welcome' & '/list': accessible à tous
-	 * '/newuser' & '/delete-user-*': Accessible uniquement à Admin
-	 * '/edit-user-*': Accessible à Admin & DBA
+	 * L'accÃ¨s aux URL est rÃ©gi comme suit:
+	 * '/' '/welcome' & '/list': accessible Ã  tous
+	 * '/newuser' & '/delete-user-*': Accessible uniquement Ã  Admin
+	 * '/edit-user-*': Accessible Ã  Admin & DBA
 	 */
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests().antMatchers("/", "/welcome", "/login", "/logout")
 				.permitAll();
 		
-		// Pour tous les rôles: Lister tous les utilisateurs
+		// Pour tous les rÃ´les: Lister tous les utilisateurs
 		http.authorizeRequests().antMatchers("/list")
 				.access("hasAnyRole('USER', 'ADMIN', 'DBA')");
 		
-		// Pour Admin seulement: Suppression et création d'un utilisateur
+		// Pour Admin seulement: Suppression et crÃ©ation d'un utilisateur
 		http.authorizeRequests().antMatchers("/newuser/**", "/delete-user-*")
 				.access("hasRole('ADMIN')");
 		
@@ -74,57 +79,61 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.defaultSuccessUrl("/list")
 				.usernameParameter("username")
 				.passwordParameter("password")
+				.failureUrl("/login?error=true")
+                .successHandler(new MyAuthenticationSuccessHandler())	               
 				.and()
 			.rememberMe()
 				.rememberMeParameter("remember-me")
 			.tokenRepository(persistentTokenRepository)
-				.tokenValiditySeconds(86400)
+				.tokenValiditySeconds(1800) 
 				.and().csrf().and().exceptionHandling()
 			.accessDeniedPage("/Access_Denied");
+		
+		http.sessionManagement().sessionFixation().migrateSession();
+		
+		http.sessionManagement().maximumSessions(1).expiredUrl("/login?expired=true");
 	}
 	
 	/**
-	 * Cette méthode permet de crypter le mot de passe dans la base de données, 
-	 * on utilisant la classe BCryptPasswordEncoder. (voir la démo - package runtime)
+	 * Cette mÃ©thode permet de crypter le mot de passe dans la base de donnÃ©es, 
+	 * on utilisant la classe BCryptPasswordEncoder. (voir la dÃ©mo - package runtime)
 	 * @return
 	 */
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	public BCryptPasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
 	/**
-	 * Puisque nous stockons les informations d'identification dans la base de données, 
+	 * Puisque nous stockons les informations d'identification dans la base de donnÃ©es, 
 	 * la configuration de DaoAuthenticationProvider avec UserDetailsService serait utile.
 	 */
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
 		authenticationProvider.setUserDetailsService(userDetailsService);
-		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		authenticationProvider.setPasswordEncoder(getPasswordEncoder());
 		return authenticationProvider;
 	}
 	
 	/**
-	 * Cette méthode fournie la fonctionnalité "RememberMe", qui permettra d'identifier l'utilisateur à travers plusieurs sessions. 
-	 * "RememberMe" ne démarre qu'après la fin de la session (avec un délai d'inactivité). 
+	 * Cette mÃ©thode fournie la fonctionnalitÃ© "RememberMe", qui permettra d'identifier l'utilisateur Ã  travers plusieurs sessions. 
+	 * "RememberMe" ne dÃ©marre qu'aprÃ¨s la fin de la session (avec un dÃ©lai d'inactivitÃ©). 
 	 * 
-	 * Et pour garder la trace des données de jetons dans la base de données, 
-	 * nous avons configuré une implémentation PersistentTokenRepository.
+	 * Et pour garder la trace des donnÃ©es de jetons dans la base de donnÃ©es, 
+	 * nous avons configurÃ© une implÃ©mentation PersistentTokenRepository.
 	 * @return
 	 */
 	@Bean
 	public PersistentTokenBasedRememberMeServices getPersistentTokenBasedRememberMeServices() {
-		PersistentTokenBasedRememberMeServices tokenBasedservice = new PersistentTokenBasedRememberMeServices(
-				"remember-me", userDetailsService, persistentTokenRepository);
-		return tokenBasedservice;
+		return new PersistentTokenBasedRememberMeServices("remember-me", userDetailsService, persistentTokenRepository);
 	}
 	
 	/**
 	 * AuthenticationTrustResolver: Indique si le jeton d'authentification transmis 
-	 * représente un utilisateur anonyme. AuthenticationTrustResolverImpl et son implémentation correspondante.  
-	 * Cette interface fournit une méthode isAnonymous (Authentication), 
-	 * qui permet aux classes intéressées de prendre en compte ce type particulier d'état d'authentification. 
+	 * reprÃ©sente un utilisateur anonyme. AuthenticationTrustResolverImpl et son implÃ©mentation correspondante.  
+	 * Cette interface fournit une mÃ©thode isAnonymous (Authentication), 
+	 * qui permet aux classes intÃ©ressÃ©es de prendre en compte ce type particulier d'Ã©tat d'authentification. 
 	 * @return
 	 */
 	@Bean
